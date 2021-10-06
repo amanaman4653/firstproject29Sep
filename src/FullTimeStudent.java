c08a497f4b5ae4825d9ec00ea23920535383a9a8
User <User@User-PC.hitronhub.home>
2021-10-06 18:54:35 -0400

    new commit third

---
+++ src/FullTimeStudent.java
@@ -0,0 +1,20 @@
+
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class FullTimeStudent {
+
+    public FullTimeStudent() {
+    }
+    private static final Logger LOG = Logger.getLogger(FullTimeStudent.class.getName());
+    
+}
